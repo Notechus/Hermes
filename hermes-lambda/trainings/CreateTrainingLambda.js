589e1beab0258c02d51a15af3715126d4a1a71ad
notechus <seba0713@gmail.com>
2019-01-05 16:30:21 +0100

    create training lambda is working

--- hermes-lambda/trainings/CreateTrainingLambda.js
+++ hermes-lambda/trainings/CreateTrainingLambda.js
@@ -1,12 +1,72 @@
-const AWS = require('aws-sdk');
-
+const AWS = require("aws-sdk");
+const uuid = require("uuid");
 const ddb = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+AWS.config.update({ region: "eu-west-1" });
+
+exports.handler = async (event, context) => {
+  if (!event.requestContext.authorizer) {
+    return errorResponse("Authorization not configured", context.awsRequestId);
+  }
+
+  const trainingId = uuid.v4();
+  console.log(`Received event ("${trainingId}"):`, event);
+  const username = event.requestContext.authorizer.claims["cognito:username"];
+
+  const requestBody = JSON.parse(event.body);
+
+  console.log("Received request body: ", requestBody);
+
+  const training = {
+    TrainingId: trainingId,
+    Coach: username,
+    runner: requestBody.runner,
+    activityDate: requestBody.activityDate,
+    creationDate: new Date().toISOString(),
+    coachNotes: requestBody.coachNotes,
+    activities: requestBody.activities,
+    importance: requestBody.importance,
+    intensity: requestBody.intensity,
+    completed: false
+  };
+
+  console.log("Saving training: ", training);
+
+  let response = await saveTraining(training, context.awsRequestId);
+  console.log("put record", response);
+
+  return response;
+};
+
+const saveTraining = (training, awsRequestId) => {
+  return ddb
+    .put({ TableName: "Trainings", Item: training })
+    .promise()
+    .then(() => {
+      console.log("Successfully saved training");
+      return {
+        statusCode: 201,
+        body: JSON.stringify(training),
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        }
+      };
+    })
+    .catch(err => {
+      console.error(err);
+      return errorResponse(err.message, awsRequestId);
+    });
+};
+
+const errorResponse = (errorMessage, awsRequestId) => {
+  return {
+    statusCode: 500,
+    body: JSON.stringify({
+      Error: errorMessage,
+      Reference: awsRequestId
+    }),
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  };
 };
